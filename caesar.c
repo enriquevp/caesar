db37510e631fa97b60aa8a764fc6aec766dbd814
Enrique Villarreal <chooseadarkday@gmail.com>
2015-02-12 08:48:21 +0100

    Removed print statement from cipher function, removed decipher function (n, style changes

--- caesar.c
+++ caesar.c
@@ -1,49 +1,28 @@
-//Caesar.c
-//Implements a basic caesar cipher. Provides ciphering and deciphering
+/* Caesar.c 
+ * Implements a basic caesar cipher. Provides ciphering and deciphering */
 
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-void cipher(char *string, int shiftkey)
-{
+void cipher(char *string, int shiftkey){
     int  i;
-    for (i=0; i < strlen(string);i++)
-    {
+    for (i=0; i < strlen(string); i++){
         string[i] += shiftkey;
+
         if (string[i] >'z')
-        {
-            string[i]-=26;
-        }
+            string[i] = string[i] % 'z';
 
         if (string[i] <'a')
-        {
             string[i] = ' ';
-        }
     }
-    printf("%s\n", string);
 }
 
-void decipher (char *string, int shiftkey)
-{
-    int i;
-
-    for (i=0; i < strlen(string);i++)
-    {
-        string[i]-=shiftkey;
-        if (string[i] <'a')
-        {
-            string[i]+=26;
-        }
-    }
-    printf("%s\n", string);
-}
-
-int main ( int argc, char *argv[])
-{
+int main ( int argc, char *argv[]){
     int shiftkey;
     shiftkey=atoi(argv[2]);
 
     cipher(argv[1], shiftkey);
+    printf("%s\n", argv[1]); /* Since it is printed after the function finishes,
+                              * argv[1] now contains the ciphered string.     */
     return 0;
 }
