e08c331c3dc685184462974b3d665eee8f9061d6
Till Hoeppner <till@hoeppner.ws>
2015-03-12 21:14:01 +0100

    Sanitize input and handle negative offset wraps

--- caesar.c
+++ caesar.c
@@ -6,24 +6,30 @@
 #include <stdlib.h>
 #include <err.h>
 
-void cipher(char *string, int offset) {
-    int i;
-    for (i = 0; i < strlen(string); i++){
-        string[i] += offset;
+void cipher(char *str, int offset) {
+    while (*str) {
+        *str = (*str - 'a' + (offset + 26)) % 26 + 'a' ;
+        str++;
+    }
+}
 
-        if (string[i] > 'z')
-            string[i] -=  26;
-
-        if (string[i] < 'a')
-            string[i] -= 26;
+// Handling two alphabets is out of scope, so restrict to lowercase.
+// Replace uppercase letters with lowercase letters.
+void sanitize(char *str) {
+    while (*str) {
+        if (*str >= 'A' && *str <= 'Z') *str = *str - 'A' + 'a';
+        if (*str < 'a' || *str > 'z') *str = '?';
+        str++;
     }
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3){
+    if (argc != 3) {
         printf("usage: %s <string> <key>\n", argv[0]);
         exit(1);
     }
+
+    sanitize(argv[1]);
 
     int offset;
     offset = atoi(argv[2]) % 26;
